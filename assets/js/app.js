6bf1c482966528bfeda7d4975d041dd27e3627d9
raghav-joshi-5 <raghav.joshi2017@gmail.com>
2024-08-30 13:31:50 +0530

    navbarrwd

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+let cl=console.log;
+
+const toggleBtn=document.getElementById('toggleBtn');
+const navbar=document.getElementById('navbar');
+const onClick= ()=>{
+    let navbarclass=navbar.className;
+    cl(navbarclass);
+    if(navbarclass.includes('active')){
+        navbar.classList.remove('active')
+    }else{
+        navbar.classList.add('active')
+    }
+}
+
+toggleBtn.addEventListener('click',onClick)
